e142c36a0a8c0b9e48a5834337bbc88528cce166
hanjaeng <hanjaeng@gmail.com>
2024-08-05 09:31:44 +0900

    첫번째 업로드

---
+++ flutter(stateless&ful)_app/d*_listview_todo_listup_app/lib/model/practice.dart
@@ -0,0 +1,16 @@
+class Practice{
+
+//Properties
+  String Name;
+  String Name2;
+  late String Name3;
+
+  // Constructor
+  Practice(
+    {
+      required this.Name,
+      required this.Name2,
+      this.Name3 = '메롱'
+    }
+  );
+}
